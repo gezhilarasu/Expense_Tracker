9d77bc45235423f0659ace044a4aa30b75a3ed9e
gezhilarasu <gezhilarasu24@gmail.com>
2025-03-12 22:18:33 +0530

    updated

--- client/src/pages/budget.jsx
+++ client/src/pages/budget.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
 import Sidebar from "../components/sidebar";
-import "./Budget.css";
+import "./budget.css";
 
 const Budget = () => {
   const [category, setCategory] = useState("");
